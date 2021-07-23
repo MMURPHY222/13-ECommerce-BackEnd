bcbf043385e5efad60ae868368727f20e3a1e1ab
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-07-23 14:30:19 -0700

    updated index.js model

--- models/index.js
+++ models/index.js
@@ -3,14 +3,35 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+// const seedTags = require('../seeds/tag-seeds');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'categoryID',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'categoryID',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // foreignKey: 'ProductTagId',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // foreignKey: 'TagId',
+});
 
 module.exports = {
   Product,
